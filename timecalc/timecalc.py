0aa2c959bc0f1b89e6022d879a950d28680fea3a
Nathan Robinson <NRobinson@rcs-uk.com>
2023-11-03 09:13:45 +0000

    finish time calc

--- timecalc/timecalc.py
+++ timecalc/timecalc.py
@@ -1,5 +1,9 @@
+from decimal import ROUND_DOWN
+from os import close
+
+
 while True:
-    select = int(input('''hello and welcome to the time calculator, please select an option
+    select = (input('''hello and welcome to the time calculator, please select an option
     1-	Add 2 times   
     2-	Find the difference between 2 times  
     3-	Convert to Hours  
@@ -13,7 +17,7 @@ while True:
           '''))
 
     match(select):
-        case 1:
+        case "1":
             time1=input("please enter the first day time in the format dd:hh:mm").split(":")
             time2=input("please enter a second time in the same format").split(":")
             result=[]
@@ -30,7 +34,7 @@ while True:
                 
             print (f"{result[0]}:{result[1]}:{result[2]}")
             
-        case 2:
+        case "2":
             time1=input("please enter the first (larger) day time in the format dd:hh:mm").split(":")
             time2=input("please enter a second time in the same format").split(":")
             result.append(int(time1[0])-int(time2[0]))
@@ -45,15 +49,53 @@ while True:
                 
             print(result[0]+":"+result[1]+":"+result[2])
                 
-        case 3:
+        case "3":
             time1=input("please enter the first day time in the format dd:hh:mm").split(":")
             hours=float(int(time1[0])*24 + int(time1[1]) + int(time1[2])/60 )
             print (hours)
             
-        case 4:
+        case "4":
             time1=input("please enter the first day time in the format dd:hh:mm").split(":")
             mins=int(int(time1[0])*24*60 + int(time1[1])*60 + int(time1[2]))
             print(mins)
             
-        case 5:
+        case "5":
             mins=float(input("enter the number of mins"))
+            hours=0
+            days=0
+            while mins>=60:
+                mins-=60
+                hours+=1
+                
+            while hours>=24:
+                hours -=24
+                days +=1
+                
+            print(f"{days}:{hours}:{mins}")
+            
+        case "6":
+            hours = float(input("enter number of hours"))
+            mins=0
+            days=0
+            whole=ROUND_DOWN(hours)
+            while hours >=24:
+                hours-=24
+                days+=1
+                
+            print(f"{days}:{whole}:{(hours-whole)*60}")
+            
+        case "7":
+            days = float (input("enter number days"))
+            hours=0
+            mins=0
+            wholeDay=ROUND_DOWN(days)
+            hours=(days-wholeDay)*24
+            wholeHour=ROUND_DOWN(hours)
+            mins=(hours-wholeHour)*60
+            print(f"{wholeDay}:{wholeHour}:{mins}")
+            
+        case "8":
+            exit()
+            
+        case _:
+            print("error invalid input")
